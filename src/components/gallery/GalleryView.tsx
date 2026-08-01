b5f809d3676a48aef05ee1ed5b201feffa7bdb4e
Efe Kırbaş <efekrbass@gmail.com>
2026-08-01 03:44:14 +0300

    fix: make gallery photos stack vertically on mobile screens

--- src/components/gallery/GalleryView.tsx
+++ src/components/gallery/GalleryView.tsx
@@ -10,7 +10,7 @@ export default function GalleryView() {
   const { gallery } = getDictionary(locale);
 
   return (
-    <Row gap="16" fillWidth>
+    <Flex direction="row" m={{ direction: "column" }} gap="16" fillWidth>
       {gallery.images.map((image, index) => (
         <Flex flex={1} key={index}>
           <Media
@@ -25,6 +25,6 @@ export default function GalleryView() {
           />
         </Flex>
       ))}
-    </Row>
+    </Flex>
   );
 }
